3b403699de3e7b40bf6de031918f092ea5b16ee2
Paweł Owerko <p.owerko@perfectgym.com>
2018-09-28 15:46:31 +0200

    Refactoring change AutomergeBotConfiguration.cs file location.

--- src/PerfectGym.AutomergeBot/AutomergeBotConfiguration.cs
+++ src/PerfectGym.AutomergeBot/AutomergeBotConfiguration.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using PerfectGym.AutomergeBot.Models;
 
-namespace PerfectGym.AutomergeBot.AutomergeBot
+namespace PerfectGym.AutomergeBot
 {
     public class AutomergeBotConfiguration
     {
