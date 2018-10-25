9d7df93565326448137d483677a5263733fc454f
Paweł Owerko <p.owerko@perfectgym.com>
2018-10-25 09:27:40 +0200

    Logging info about work done by PullRequestMergeRetryier

--- src/PerfectGym.AutomergeBot/MergingBranches/PullRequestMergeRetryier.cs
+++ src/PerfectGym.AutomergeBot/MergingBranches/PullRequestMergeRetryier.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Octokit;
 using PerfectGym.AutomergeBot.Models;
@@ -14,22 +15,29 @@ namespace PerfectGym.AutomergeBot.MergingBranches
 
     public class PullRequestMergeRetryier : IPullRequestMergeRetryier
     {
+        private readonly ILogger<PullRequestMergeRetryier> _logger;
         private readonly AutomergeBotConfiguration _cfg;
         private readonly IMergePerformer _mergePerformer;
 
         public PullRequestMergeRetryier(
+            ILogger<PullRequestMergeRetryier> logger,
             IOptionsMonitor<AutomergeBotConfiguration> cfg,
             IMergePerformer mergePerformer)
         {
+            _logger = logger;
             _cfg = cfg.CurrentValue;
             _mergePerformer = mergePerformer;
         }
 
         public void RetryMergePullRequestsCreatedBefore(PushInfoModel pushInfo, IRepositoryConnectionContext repoContext)
         {
-            var openPullRequestsTargetingBranch = GetOpenPullRequestsTargetingBranch(pushInfo, repoContext);
-            if (Enumerable.Any<PullRequest>(openPullRequestsTargetingBranch))
+            var targetBranchName = pushInfo.GetPushedBranchName().Name;
+            _logger.LogInformation("Retrying merging pull requests created by AutomergeBot before and not merged yet to branch {targetBranch}", targetBranchName);
+
+            var openPullRequestsTargetingBranch = GetOpenPullRequestsTargetingBranch(repoContext, targetBranchName);
+            if (openPullRequestsTargetingBranch.Any())
             {
+                _logger.LogDebug("There is {openPullRequestsCount} pull requests which potentially could be merged", openPullRequestsTargetingBranch.Count);
                 foreach (var pullRequest in openPullRequestsTargetingBranch)
                 {
                     _mergePerformer.TryMergeExistingPullRequest(pullRequest, repoContext);
@@ -37,9 +45,8 @@ namespace PerfectGym.AutomergeBot.MergingBranches
             }
         }
 
-        private List<PullRequest> GetOpenPullRequestsTargetingBranch(PushInfoModel pushInfo, IRepositoryConnectionContext repoContext)
+        private List<PullRequest> GetOpenPullRequestsTargetingBranch(IRepositoryConnectionContext repoContext, string targetBranchName)
         {
-            var targetBranchName = pushInfo.GetPushedBranchName().Name;
             var openPullRequestsTargetingBranch = repoContext
                 .GetOpenPullRequests()
                 .Where(pr => pr.Base.Ref == targetBranchName)
